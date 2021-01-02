51feb76e4b024401bdc6177ca118ae0f2588820a
yangfanfinland <yangfanfinland@gmail.com>
2021-01-02 03:28:38 +0200

    app footer

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,4 @@
 export * from './components/header';
+export * from './components/footer';
 export * from './components/search';
 export * from './shared.module';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header';
+import { FooterComponent } from './components/footer';
 import { SearchComponent } from './components/search';
 
 @NgModule({
-  declarations: [HeaderComponent, SearchComponent],
+  declarations: [HeaderComponent, FooterComponent, SearchComponent],
   imports: [CommonModule, FormsModule],
-  exports: [CommonModule, FormsModule, HeaderComponent, SearchComponent],
+  exports: [CommonModule, FormsModule, HeaderComponent, FooterComponent, SearchComponent],
 })
 export class SharedModule {}
