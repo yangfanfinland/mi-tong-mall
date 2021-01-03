c52d0bda17f7896bc9a62c52b68f93fac2fdd43b
yangfanfinland <yangfanfinland@gmail.com>
2021-01-03 11:48:04 +0200

    shoppingcart operations

--- src/app/cart/components/cart-container/cart-container.component.ts
+++ src/app/cart/components/cart-container/cart-container.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product, ProductDetail } from 'src/app/shared';
 import { CartService } from 'src/app/cart';
-import { map, filter, switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -35,9 +35,52 @@ export class CartContainerComponent implements OnInit {
     this.filter(data);
     // 缓存购物车信息
     this.data.cartInfo = data;
-
   }
   filter(data: any) {
     data.notEmpty = !!data.cartProductVoList.length;
+  }
+  cartSelect(value: boolean, productId: number) {
+    // 选中
+    if (value) {
+      this.cart$ = this.service.selectProduct(productId).pipe(map(res => res.data));
+    } else {
+      this.cart$ = this.service.unselectProduct(productId).pipe(map(res => res.data));
+    }
+  }
+  cartSelectAll(value: boolean) {
+    if (value) {
+      this.cart$ = this.service.selectAllProduct().pipe(map(res => res.data));
+    } else {
+      this.cart$ = this.service.unselectAllProduct().pipe(map(res => res.data));
+    }
+  }
+  plus(quantity: number, maxQuantity: number, productId: number) {
+    if ((quantity+1) >= maxQuantity) {
+      alert('该商品数量已达到上限');
+      return;
+    }
+    this.cart$ = this.service.updateProduct(productId, quantity+1).pipe(map(res => res.data));
+  }
+  minus(quantity: number, productId: number) {
+    if ((quantity-1) <= 0) {
+      return;
+    }
+    this.cart$ = this.service.updateProduct(productId, quantity-1).pipe(map(res => res.data));
+  }
+  cartDelete(productId: number) {
+    if (window.confirm('确认要删除该商品？')) {
+      this.cart$ = this.service.deleteProduct(productId.toString()).pipe(map(res => res.data));
+    }
+  }
+  deleteSelected(cart: any) {
+    if (window.confirm('确认要删除该商品？')) {
+      let arrProductIds = [];
+      for (const productVo of cart.cartProductVoList) {
+        if (productVo.productChecked) {
+          arrProductIds.push(productVo.productId);
+        }
+      }
+      this.cart$ = this.service.deleteProduct(arrProductIds.join(',')).pipe(map(res => res.data));
+    }
   }
 }
