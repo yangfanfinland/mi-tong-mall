c44588550920a05bf1509c39978ee526f861c6ba
yangfanfinland <yangfanfinland@gmail.com>
2021-01-12 23:09:18 +0200

    enter key up for product search

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,25 +1,25 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-search',
-    templateUrl: './search.component.html',
-    styleUrls: ['./search.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchComponent implements OnInit {
-    keyword = new FormControl('')
+  keyword = new FormControl('');
 
-    constructor(
-        private router: Router,
-      ) {}
+  constructor(private router: Router) {}
 
-    ngOnInit() {}
+  ngOnInit() {}
 
-    search() {
-        if (this.keyword.value) {
-            this.router.navigate(['/product'], { queryParams: { keyword: this.keyword.value }})
-        }
+  search() {
+    if (this.keyword.value) {
+      this.router.navigate(['/product'], {
+        queryParams: { keyword: this.keyword.value },
+      });
     }
+  }
 }
