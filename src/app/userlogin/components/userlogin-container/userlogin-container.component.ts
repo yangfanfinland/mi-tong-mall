67dad6327fdf2ec0f461db9a8a9dda25f093cbd7
yangfanfinland <yangfanfinland@gmail.com>
2021-01-16 00:09:57 +0200

    add validation for user login

--- src/app/userlogin/components/userlogin-container/userlogin-container.component.ts
+++ src/app/userlogin/components/userlogin-container/userlogin-container.component.ts
@@ -4,8 +4,13 @@ import { Observable } from 'rxjs';
 import { Product, ProductDetail } from 'src/app/shared';
 import { UserService } from 'src/app/userlogin';
 import { take } from 'rxjs/operators';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { add } from '../../../store/user.actions';
 
@@ -16,26 +21,41 @@ import { add } from '../../../store/user.actions';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserLoginContainerComponent implements OnInit {
-  username = new FormControl('')
-  password = new FormControl('')
-  user$: Observable<any>
+  user$: Observable<any>;
+  loginForm: FormGroup;
+  submitted = false;
 
   constructor(
     private service: UserService,
     private store: Store<{ user: any }>,
     private router: Router,
+    private formBuilder: FormBuilder
   ) {
-    this.user$ = store.select('user')
+    this.user$ = store.select('user');
   }
 
   ngOnInit(): void {
-
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
-  login() {
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.user$.pipe(take(1)).subscribe(value => {
       if (!value.username) {
-        this.service.login({ username: this.username.value, password: this.password.value }).subscribe(user => {
+        this.service.login({ username: this.loginForm.value.username, password: this.loginForm.value.password }).subscribe(user => {
           this.store.dispatch(add({ payload: (user as any).data }))
           this.router.navigate(['/home']);
         })
