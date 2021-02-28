15a7d23ba46be4f011083ee7d030409d8907a072
yangfanfinland <yangfanfinland@gmail.com>
2021-02-28 13:25:18 +0200

    translate category name to english

--- src/app/modules/home/components/home-container/home-container.component.ts
+++ src/app/modules/home/components/home-container/home-container.component.ts
@@ -44,151 +44,151 @@ export class HomeContainerComponent implements OnInit {
 
   floors = [
     {
-      title: 'F1 家用电器',
+      title: 'F1 Household appliances',
       categories: [
         {
           id: '100006',
-          text: '双开门冰箱',
+          text: 'Fridge',
           image: '/assets/image/floor/floor1-1.jpg',
         },
         {
           id: '100007',
-          text: '电视',
+          text: 'TV',
           image: '/assets/image/floor/floor1-2.jpg',
         },
         {
           id: '100008',
-          text: '洗衣机',
+          text: 'Washing machine',
           image: '/assets/image/floor/floor1-3.jpg',
         },
         {
           id: '100009',
-          text: '空调',
+          text: 'Air conditioning',
           image: '/assets/image/floor/floor1-4.jpg',
         },
         {
           id: '100010',
-          text: '热水器',
+          text: 'Water heater',
           image: '/assets/image/floor/floor1-5.jpg',
         },
       ],
     },
     {
-      title: 'F2 数码3C',
+      title: 'F2 Digital 3C',
       categories: [
         {
           id: '100011',
-          text: '笔记本电脑',
+          text: 'Laptop',
           image: '/assets/image/floor/floor2-1.jpg',
         },
         {
           id: '100012',
-          text: '手机',
+          text: 'Cell phone',
           image: '/assets/image/floor/floor2-2.jpg',
         },
         {
           id: '100013',
-          text: '平板电脑',
+          text: 'Tablet',
           image: '/assets/image/floor/floor2-3.jpg',
         },
         {
           id: '100014',
-          text: '数码相机',
+          text: 'Digital camera',
           image: '/assets/image/floor/floor2-4.jpg',
         },
         {
           id: '100015',
-          text: '3C配件',
+          text: '3C accessories',
           image: '/assets/image/floor/floor2-5.jpg',
         },
       ],
     },
     {
-      title: 'F3 服装箱包',
+      title: 'F3 Clothing luggage',
       categories: [
         {
           id: '100016',
-          text: '女装',
+          text: "Women's clothing",
           image: '/assets/image/floor/floor3-1.jpg',
         },
         {
           id: '100017',
-          text: '帽子专区',
+          text: 'Hat area',
           image: '/assets/image/floor/floor3-2.jpg',
         },
         {
           id: '100018',
-          text: '旅行箱',
+          text: 'Suitcase',
           image: '/assets/image/floor/floor3-3.jpg',
         },
         {
           id: '100019',
-          text: '手提包',
+          text: 'handbag',
           image: '/assets/image/floor/floor3-4.jpg',
         },
         {
           id: '100020',
-          text: '保暖内衣',
+          text: 'Thermal underwear',
           image: '/assets/image/floor/floor3-5.jpg',
         },
       ],
     },
     {
-      title: 'F4 食品生鲜',
+      title: 'F4 Fresh food',
       categories: [
         {
           id: '100021',
-          text: '最爱零食',
+          text: 'Favorite snack',
           image: '/assets/image/floor/floor4-1.jpg',
         },
         {
           id: '100022',
-          text: '生鲜',
+          text: 'Fresh food',
           image: '/assets/image/floor/floor4-2.jpg',
         },
         {
           id: '100023',
-          text: '半成品菜',
+          text: 'Semi-finished dishes',
           image: '/assets/image/floor/floor4-3.jpg',
         },
         {
           id: '100024',
-          text: '速冻专区',
+          text: 'Frozen zone',
           image: '/assets/image/floor/floor4-4.jpg',
         },
         {
           id: '100025',
-          text: '进口牛奶',
+          text: 'Imported milk',
           image: '/assets/image/floor/floor4-5.jpg',
         },
       ],
     },
     {
-      title: 'F5 酒水饮料',
+      title: 'F5 Drinks',
       categories: [
         {
           id: '100026',
-          text: '白酒',
+          text: 'White spirit',
           image: '/assets/image/floor/floor5-1.jpg',
         },
         {
           id: '100027',
-          text: '红酒',
+          text: 'Red wine',
           image: '/assets/image/floor/floor5-2.jpg',
         },
         {
           id: '100028',
-          text: '饮料',
+          text: 'Drinks',
           image: '/assets/image/floor/floor5-3.jpg',
         },
         {
           id: '100029',
-          text: '调制鸡尾酒',
+          text: 'Cocktail',
           image: '/assets/image/floor/floor5-4.jpg',
         },
         {
           id: '100030',
-          text: '进口洋酒',
+          text: 'Imported wine',
           image: '/assets/image/floor/floor5-5.jpg',
         },
       ],
